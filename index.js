6083728b66f46653602917f1dba26125928c218e
+ <+>
2021-09-01 22:04:39 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(star = '*') {
+    return function compliment(adjective = 'special') {
+        return `You are ${star}${adjective}${star}!`
+    }
+}
+console.log(wrapAdjective('*')('a hard worker'));
